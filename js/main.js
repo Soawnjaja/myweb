69380be607a7c614dc88ad61f71c2820d077cf44
Alex Wishinkiy <basstraper@gmail.com>
2020-07-02 19:34:59 +0300

    New test
    no bg

---
+++ js/main.js
@@ -0,0 +1,8 @@
+new WOW().init();
+var mySwiper = new Swiper('.swiper-container', {
+    pagination: {
+        el: '.project-pagination',
+        bulletClass: 'project-bullet',
+        bulletActiveClass: 'project-bullet-active'
+    },
+});
